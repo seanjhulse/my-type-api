efe37fb50aebced7675d3f5d83590e29f772502b
Gandalf the Grey <gandalf@localhost.localdomain>
2018-05-10 00:18:07 -0500

    minor database configurations and temporary carrierwave info

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# my-type-api
 An API to send photos, login data, and location data
+
+## Models.
+
+### Humans.
+
+*Contain*
+- Age
+- Biography
+- Name
+- Photo_ids
+- Ratings
+
+### Photos.
+
+*Contain*
+- Photo_URLs 
+
+### Ratings.
+
+*Contain*
+- Photo_id
+- Rating
+- User_id
+
+
+Sally provides a photo. Bob rates the photo. Bob's rating is tied to the photo. The algorithm compares matrix of photos with weights of ratings in Bob's profile. Finds photos closely matches to Bob's rating.
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+    include Response
+    include ExceptionHandler
+    before_action :set_user, except: [:index, :new, :create]
+
+    def index
+        @users = User.all
+        json_response(@users)
+    end
+    def new
+        @user = User.new
+        json_response(@user)
+    end
+    def create
+        @user = User.new(user_params)
+        @user.save
+        json_response(@user)
+    end
+    def show
+        json_response(@user)
+    end
+    def edit
+        json_response(@user)
+    end
+    def update
+        json_response(@user)
+    end
+    def destroy
+        @user.destroy
+        json_response(@user)
+    end
+
+    private
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :age, :biography)
+    end
+end
